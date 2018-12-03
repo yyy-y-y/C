cb1b19fa4489af74be2b4cb2b2b12b4698dd7a50
yyy-y-y <45565489+yyy-y-y@users.noreply.github.com>
2018-12-04 01:01:44 +0900

    変更した

---
+++ swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define sw(x,y) (x+=x,y=x-y,x-=y)
+
+int main(void){  
+    int x,y;
+
+    x=3;
+    y=5;
+    printf("x=%d\n",x); 
+    sw(x,y);
+    printf("x=%d\n",x);
+    return 0;
+}
